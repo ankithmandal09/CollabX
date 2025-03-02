6434816248668dda8e80ab55c530578218afed4d
Raj Ghosh <89258247+Rajghosh786@users.noreply.github.com>
2025-03-02 10:23:47 +0530

    login auuthentication connected across the pages

--- Frontend/collabX/src/Component/Register.jsx
+++ Frontend/collabX/src/Component/Register.jsx
@@ -55,7 +55,8 @@ const Register = () => {
         password: credentials.password
       };
 
-      const response = await axios.post('https://hackathonfeb2025users-default-rtdb.asia-southeast1.firebasedatabase.app/.json', credentialsPayload);
+      // const response = await axios.post('https://hackathonfeb2025users-default-rtdb.asia-southeast1.firebasedatabase.app/.json', credentialsPayload);
+      const response = await axios.post(`http://localhost:8090/users`,credentialsPayload);
       console.log(response);
     } catch (error) {
       console.error(error);
@@ -76,8 +77,8 @@ const Register = () => {
         skills: formData.skills
       };
 
-      const response = await axios.post('https://hackathon-form-data-default-rtdb.asia-southeast1.firebasedatabase.app/.json', formPayload);
-      // const response = await axios.post(`https://github.com/ankithmandal09/CollabX/blob/bharathraj/Backend/routes/profiles.route.js/${formData.username}`,formPayload);
+      // const response = await axios.post('https://hackathon-form-data-default-rtdb.asia-southeast1.firebasedatabase.app/.json', formPayload);
+      const response = await axios.post(`http://localhost:8090/profiles/${credentials.username}`,formPayload);
       console.log(response);
     } catch (error) {
       console.error(error);
