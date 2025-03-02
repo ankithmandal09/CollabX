786de0b76aae283f13a32d33aab8658afbb49f9f
Raj Ghosh <89258247+Rajghosh786@users.noreply.github.com>
2025-03-02 18:20:01 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -1,47 +1,3 @@
-// const express = require("express");
-// const cors = require("cors");
-// const helmet = require("helmet");
-// require("dotenv").config();
-
-// const connection = require("./config/db.js");
-// const CollabXRouter = require("./routes/collabx.route.js");
-// const UserRouter = require("./routes/user.route.js");
-// const ProfileRouter = require("./routes/profile.route.js");
-// const PostRouter = require("./routes/post.route.js");
-
-// const PORT = process.env.PORT;
-
-// const app = express();
-
-// app.use(express.json());
-// app.use(cors({
-//     "origin": "*",
-//     "methods": "GET,HEAD,PUT,PATCH,POST,DELETE"
-// }));
-
-// app.use(helmet());
-
-// app.get("/", (req, res) => {
-//     res.status(200).json({message: "Healthy"});
-// });
-
-// app.use("/collabx", CollabXRouter);
-// app.use("/users", UserRouter);
-// app.use("/profiles", ProfileRouter);
-// app.use("/posts", PostRouter);
-
-// app.listen(PORT, async (err) => {
-//     if(err){
-//         console.error("Error connecting to the server.\n", err);
-//     }
-//     console.log("Successfully connected to the server.");
-//     try {
-//         await connection;
-//         console.log("Successfully connected to the database.");
-//     } catch (error) {
-//         console.error("Error connecting to the databade.\n", error);   
-//     }
-// });
 const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
@@ -57,19 +13,16 @@ const PORT = process.env.PORT;
 
 const app = express();
 
+app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:5173',
-  credentials: true,
+    "origin": "*",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE"
 }));
-
-app.options('*', cors());
 
 app.use(helmet());
 
-app.use(express.json());
-
 app.get("/", (req, res) => {
-    res.status(200).json({ message: "Healthy" });
+    res.status(200).json({message: "Healthy"});
 });
 
 app.use("/collabx", CollabXRouter);
@@ -78,7 +31,7 @@ app.use("/profiles", ProfileRouter);
 app.use("/posts", PostRouter);
 
 app.listen(PORT, async (err) => {
-    if (err) {
+    if(err){
         console.error("Error connecting to the server.\n", err);
     }
     console.log("Successfully connected to the server.");
@@ -86,6 +39,53 @@ app.listen(PORT, async (err) => {
         await connection;
         console.log("Successfully connected to the database.");
     } catch (error) {
-        console.error("Error connecting to the database.\n", error);   
+        console.error("Error connecting to the databade.\n", error);   
     }
 });
+// const express = require("express");
+// const cors = require("cors");
+// const helmet = require("helmet");
+// require("dotenv").config();
+
+// const connection = require("./config/db.js");
+// const CollabXRouter = require("./routes/collabx.route.js");
+// const UserRouter = require("./routes/user.route.js");
+// const ProfileRouter = require("./routes/profile.route.js");
+// const PostRouter = require("./routes/post.route.js");
+
+// const PORT = process.env.PORT;
+
+// const app = express();
+
+// app.use(cors({
+//   origin: 'http://localhost:5173',
+//   credentials: true,
+// }));
+
+// app.options('*', cors());
+
+// app.use(helmet());
+
+// app.use(express.json());
+
+// app.get("/", (req, res) => {
+//     res.status(200).json({ message: "Healthy" });
+// });
+
+// app.use("/collabx", CollabXRouter);
+// app.use("/users", UserRouter);
+// app.use("/profiles", ProfileRouter);
+// app.use("/posts", PostRouter);
+
+// app.listen(PORT, async (err) => {
+//     if (err) {
+//         console.error("Error connecting to the server.\n", err);
+//     }
+//     console.log("Successfully connected to the server.");
+//     try {
+//         await connection;
+//         console.log("Successfully connected to the database.");
+//     } catch (error) {
+//         console.error("Error connecting to the database.\n", error);   
+//     }
+// });
