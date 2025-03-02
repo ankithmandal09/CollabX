2798f2787fc75ca1b58aac673ed3068140af694c
Ankith Mandal <ankithmandal36@gmail.com>
2025-03-02 18:29:08 +0530

    Update main.jsx

--- Frontend/collabX/src/main.jsx
+++ Frontend/collabX/src/main.jsx
@@ -2,11 +2,14 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './Context/AuthContext.jsx';
+import PostProvider from './Context/PostContext.jsx';
 
 createRoot(document.getElementById("root")).render(
-  <AuthProvider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </AuthProvider>
+  <PostProvider>
+    <AuthProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AuthProvider>
+  </PostProvider>
 );
